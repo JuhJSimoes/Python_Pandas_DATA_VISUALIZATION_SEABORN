e54362b4be438131e51c07f388f5123f2579dfca
JuhJSimoes <julianajsimoes@gmail.com>
2022-11-29 05:01:38 -0300

    Aula_291122 Seaborn

--- exemplos.py
+++ exemplos.py
@@ -15,5 +15,24 @@ dados = dados.rename(columns=colunas)
 print(dados, '\n')
 
 paises = {'Belgium':'Bélgica', 'India':'Índia', 'Japan':'Japão'}
-dados.Country = dados.Country.map(paises)
+dados['País'] = dados['País'].map(paises)
 print(dados, '\n')
+
+
+
+boletim = ({
+    'Aluno' : ['Márcia', 'Lucas', 'Ana', 'Flávio'],
+    'Nota_1' : [10, 8, 5, 4],
+    'Nota_2' : [7, 4, 8, 5],
+    'Nota_3' : [6, 7, 4, 9]
+})
+
+dados = pd.DataFrame(boletim)
+
+print(dados, '\n')
+
+dados['Média'] = ((dados.Nota_1 + dados.Nota_2 + dados.Nota_3) / 3).round(2)
+
+print(dados, '\n')
+
+
--- gorjetas.py
+++ gorjetas.py
@@ -1,6 +1,8 @@
 #Importando base de dados
 
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 dados = pd.read_csv('tips.csv')
 print(dados.head(), '\n')
@@ -48,3 +50,33 @@ hora = {
 
 gorjetas['hora_do_dia'] = gorjetas['hora_do_dia'].map(hora)
 print(gorjetas.head(), '\n')
+
+#Analise 1 - valor da conta e gorjeta
+'''valor_gorjeta = sns.scatterplot(x = 'valor_da_conta', y = 'gorjeta', data = gorjetas)
+plt.show() #Visualmente o valor da gorjeta aumenta conforme aumenta o valor da conta'''
+
+
+print('A base de dados contém {} registros \n'. format(gorjetas.shape[0]))
+print('Registros não nulos:')
+print(gorjetas.count(), '\n')
+
+
+#criando campo porcentagem
+print(gorjetas.info(),'\n')
+gorjetas['porcentagem'] = gorjetas['gorjeta']/gorjetas['valor_da_conta']
+gorjetas['porcentagem'] = gorjetas['porcentagem'].round(2)
+print(gorjetas.head(), '\n')
+
+porcentagem_conta = sns.scatterplot(x = 'valor_da_conta', y = 'porcentagem', data = gorjetas)
+porcentagem_conta.get_figure()
+porcentagem_conta.figure.suptitle('Valor da conta x Gorjeta')
+plt.show()
+
+#visualmente p valor da conta nao é proporcional ao valor dar gorjeta
+
+
+'''porcentagem_conta_linha = sns.relplot(x = 'valor_da_conta', y = 'porcentagem', kind='line', data = gorjetas)
+plt.show()'''
+
+'''sns.lmplot(x = 'valor_da_conta', y = 'porcentagem', data = gorjetas)
+plt.show()'''
